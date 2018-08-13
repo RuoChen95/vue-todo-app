ec9457d58f33dcc74f02e1572788a51cb2f39571
ruochen.xie <ruochen.xie@bitmain.com>
2018-08-13 21:13:25 +0800

    first commitment

---
+++ vue-todo-app2/src/components/List.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="list">
+      <button @click="completeAll()">Complete All</button>
+      <input v-model="data"/>
+      <button @click="add()">add</button>
+      <div>
+          <div v-for="(components, index) in list" :key="index" class="section" :class="{'show': components.show}">
+              <div class="content">
+                  <!--checked; priority-->
+                  <div class="condition">
+                      <input type="checkbox" v-model="components.checked" @click="toggleCheckCondition(index)"/>
+                  </div>
+                  <p :class="{'checked': components.checked}">{{components.value}}</p>
+              </div>
+              <div class="buttons">
+                  <button class="delete" @click="deleteSection(index)">delete</button>
+              </div>
+          </div>
+      </div>
+      <div class="banner">
+          <p>{{leftAmount}} item left</p>
+          <div class="condition">
+              <div @click="condition = 'all'" :class="{'active': this.condition == 'all'}">All</div>
+              <div @click="condition = 'active'" :class="{'active': this.condition == 'active'}">Active</div>
+              <div @click="condition = 'completed'" :class="{'active': this.condition == 'completed'}">Completed</div>
+          </div>
+          <button @click="clear()" :class="{'show': hasCompleted}" class="clearCompleted">Clear Completed</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props: {},
+  data() {
+    return {
+      data: '',
+      list: JSON.parse(localStorage.getItem('vue-todo')) || [],
+      condition: 'all',
+    };
+  },
+  watch: {
+    list: function(val) {
+      localStorage.setItem('vue-todo', JSON.stringify(val));
+    },
+    condition: function(val) {
+      if (val == 'all') {
+        this.list.forEach(function(element) {
+          element.show = true;
+        });
+      } else if (val == 'active') {
+        this.list.forEach(function(element) {
+          if (element.checked == true) {
+            element.show = false;
+          } else {
+            element.show = true;
+          }
+        });
+      } else if (val == 'completed') {
+        this.list.forEach(function(element) {
+          if (element.checked == true) {
+            element.show = true;
+          } else {
+            element.show = false;
+          }
+        });
+      }
+    },
+  },
+  computed: {
+    leftAmount: function() {
+      let total = 0;
+      this.list.forEach(function(element) {
+        if (element.checked == false) {
+          total = total + 1;
+        }
+      });
+      return total;
+    },
+    hasCompleted: function() {
+      let length = 0;
+      this.list.forEach(function(element) {
+        if (element.checked != true) {
+          length++;
+        }
+      });
+      if (length == this.list.length) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+  methods: {
+    add: function() {
+      this.list.push({ value: this.data, checked: false, show: true });
+      this.data = '';
+    },
+    deleteSection: function(i) {
+      this.list.splice(i, 1);
+    },
+    toggleCheckCondition: function(i) {
+      this.list[i].checked = !this.list[i].checked;
+    },
+    completeAll() {
+      this.list.forEach(function(element) {
+        element.checked = true;
+      });
+    },
+    clear() {
+      let arr = [];
+      this.list.forEach(function(element) {
+        if (element.checked == false) {
+          arr.push(element);
+        }
+      });
+      this.list = arr;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" type="text/less">
+div.list {
+  div {
+    div.section {
+      display: none;
+      justify-content: space-between;
+      align-items: center;
+      border: 1px solid rosybrown;
+      &.show {
+        display: flex;
+      }
+      &:hover {
+        div.buttons {
+          display: flex;
+        }
+      }
+      div.content {
+        display: flex;
+        align-items: center;
+        p.checked {
+          text-decoration: line-through;
+        }
+      }
+      div.buttons {
+        display: none;
+      }
+    }
+  }
+  div.banner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    div.condition {
+      display: flex;
+      > div.active {
+        border: 1px solid rosybrown;
+        border-radius: 3px;
+      }
+    }
+    button.clearCompleted {
+      visibility: hidden;
+      &.show {
+        visibility: visible;
+      }
+    }
+  }
+}
+</style>
